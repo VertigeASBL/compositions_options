fcead17294d9eb6b96eb2275611caacc04c2c1f6
Michel Bystranowski <michel@vertige.org>
2015-03-20 12:18:17 +0100

    commit initial - généré par la Fabrique

---
+++ compositions_options_administrations.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Fichier gérant l'installation et désinstallation du plugin Options des compositions
+ *
+ * @plugin     Options des compositions
+ * @copyright  2015
+ * @author     Michel @ Vertige ASBL
+ * @licence    GNU/GPL
+ * @package    SPIP\Compositions_options\Installation
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+
+/**
+ * Fonction d'installation et de mise à jour du plugin Options des compositions.
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @param string $version_cible
+ *     Version du schéma de données dans ce plugin (déclaré dans paquet.xml)
+ * @return void
+**/
+function compositions_options_upgrade($nom_meta_base_version, $version_cible) {
+    $maj = array();
+
+    include_spip('base/upgrade');
+    maj_plugin($nom_meta_base_version, $version_cible, $maj);
+}
+
+
+/**
+ * Fonction de désinstallation du plugin Options des compositions.
+ *
+ * @param string $nom_meta_base_version
+ *     Nom de la meta informant de la version du schéma de données du plugin installé dans SPIP
+ * @return void
+**/
+function compositions_options_vider_tables($nom_meta_base_version) {
+
+    effacer_meta($nom_meta_base_version);
+}
---
+++ lang/paquet-compositions_options_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+    // C
+    'compositions_options_description' => '',
+    'compositions_options_nom' => 'Options des compositions',
+    'compositions_options_slogan' => 'Permettre de gérer des options pour les compositions',
+);
